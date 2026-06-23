d3c8c1f853a2c808aee89d80fba27580e787f7c9
windecay <4453636@qq.com>
2026-06-23 12:31:31 +0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='simpleai_base',
-    version='0.3.46',
+    version='0.3.47',
     packages=['simpleai_base'],
     install_requires=[
         'cbor2',
